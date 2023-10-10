43beee828a4bb8195f5dc85b47716882b16c464f
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-10-10 12:07:23 +0200

    added comments

--- Program.cs
+++ Program.cs
@@ -10,13 +10,22 @@ namespace Labb5_OOP
 
         static void Main(string[] args)
         {
+            CreateAndPrint();
+        }
+
+        private static void CreateAndPrint()
+        {
+            //Creates 3 circles with radius of 1,5 and 6
             Circle circle = new Circle(1);
             Circle circle1 = new Circle(5);
             Circle circle2 = new Circle(6);
 
+            //creates 2 triangles with equal sides of 5 and 9
             Triangle triangle = new Triangle(5);
-            Triangle triangle1 = new Triangle(6);
+            Triangle triangle1 = new Triangle(9);
 
+
+            //prints information of the shapes
             PrintShapeInfo(circle);
 
             PrintShapeInfo(circle1);
@@ -41,6 +50,7 @@ namespace Labb5_OOP
             Console.WriteLine();
         }
 
+        //Overloaded Method to print for triangles
         private static void PrintShapeInfo(Triangle triangle)
         {
             Console.WriteLine($"Triangle side: {triangle.GetSide()}");
@@ -48,21 +58,26 @@ namespace Labb5_OOP
         }
     }
 
+    
     public class Circle
     {
+
         int _Radius;
 
+        //Constructor with input for radius
         public Circle(int radius)
         {
             _Radius = radius;
 
         }
 
+        //Calculates area using radius entered in constructor
         public double GetArea()
         {
             return _Radius * _Radius * Math.PI;
         }
 
+        //Calculates Circumference using radius entered in constructor
         public double GetCircumference()
         {
             return 2 * Math.PI * _Radius;
@@ -85,18 +100,25 @@ namespace Labb5_OOP
         }
     }
         
+
+    //Class for a Triangle with equal sides
     public class Triangle
     {
         int _Side;
+
+        //Constructor with input for lenght of triangle side
         public Triangle(int side)
         {
             _Side = side;
         }
 
+        //Calculates area using triangle side entered in constructor
         public double GetTriangleArea()
         {
             return MathF.Sqrt(3) * (_Side * _Side) / 4;
         }
+
+        //Gets the lenght of a side for the print method
         public double GetSide() 
         {
             return _Side;
